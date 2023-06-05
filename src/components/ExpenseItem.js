ceab5723d25859adaf85ffc605f36a60cfb4b124
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-06-04 19:10:05 -0500

    Add  some content

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
-function ExpenseItem () {
-    return <h2>Expense Item</h2>
+function ExpenseItem() {
+  return (
+    <div>
+      <div>June 4th 2023</div>
+      <div>
+        <h2>House rent</h2>
+        <div>$700</div>
+      </div>
+    </div>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
