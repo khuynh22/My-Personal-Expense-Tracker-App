e96728135519b98149ed621b226962d34bb0c063
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-06-05 19:52:30 -0500

    Change to dynamic Data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,14 @@
 import "./ExpenseItem.css";
 function ExpenseItem() {
+  const expenseDate = new Date(2023, 6, 4);
+  const expenseTitle = 'House rent';
+  const expenseAmount = 700
   return (
     <div className="expense-item">
-      <div>June 4th 2023</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>House rent</h2>
-        <div className = "expense-item__price">$700</div>
+        <h2>{expenseTitle}</h2>
+        <div className = "expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
