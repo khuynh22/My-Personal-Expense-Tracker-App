a20e2ed559e78b9963a98673fcff112c6dcdefaf
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-06-07 12:15:17 -0500

    Update Card and create composition

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,8 +1,10 @@
+import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+
 function Expense(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -23,7 +25,7 @@ function Expense(props) {
         amount={props.items[3].amount}
         date={props.items[3].date}
       ></ExpenseItem>
-    </div>
+    </Card>
   );
 }
 
