19bce380c59f29cdebc47476154267ba839a21a2
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-06-08 16:02:59 -0500

    button and event handling - start

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,26 @@
 import ExpenseDate from "./ExpenseDate";
 import React from "react";
 import "./ExpenseItem.css";
-import Card from "../UI/Card"
+import Card from "../UI/Card";
 function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log("Clicked!");
+  };
+
   return (
     <Card className="expense-item">
-      <ExpenseDate date = {props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button
+        onClick={() => {
+          clickHandler();
+        }}
+      >
+        ChangeTitle
+      </button>
     </Card>
   );
 }
