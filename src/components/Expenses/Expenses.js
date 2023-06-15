bb5ba0d055580d2c0f78acd6113d3adee72bde13
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-06-15 12:46:50 -0500

    change and update return list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import Card from "../UI/Card";
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpenseFilter";
+import ExpenseList from "./ExpenseList";
 
 function Expense(props) {
   const [filteredYear, setFilteredYear] = useState("2022");
@@ -14,17 +14,7 @@ function Expense(props) {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
+  
   return (
     <div>
       <Card className="expenses">
@@ -32,7 +22,7 @@ function Expense(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expensesContent}
+        <ExpenseList items = {filteredExpenses}/>
       </Card>
     </div>
   );
